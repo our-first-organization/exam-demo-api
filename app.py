e5c937489ba59346948883b44eb5ff351287a4e9
Tan Wattikorn <77262463+Tantalumm@users.noreply.github.com>
2025-02-24 20:52:44 +0700

    commit

--- app.py
+++ app.py
@@ -2,13 +2,25 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/getcode', methods=['GET'])
-def get_code():
-    return "0000"
-
-@app.route('/plus/<int:a>/<int:b>', methods=['GET'])
-def plus(a, b):
-    return str(a + b), 200
+@app.route('/is_prime/<int:x>', methods=['GET'])
+def is_prime(x):
+    if x <= 1:
+        return 'false'
+    for i in range(2, int(x**0.5) + 1):
+        if x % i == 0:
+            return 'false'
+    return 'true'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081)
+    app.run(host='0.0.0.0', port = 8081)
+
+# @app.route('/getcode', methods=['GET'])
+# def get_code():
+#     return "0000"
+
+# @app.route('/plus/<int:a>/<int:b>', methods=['GET'])
+# def plus(a, b):
+#     return str(a + b), 200
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=8081)
--- test.py
+++ test.py
@@ -6,11 +6,23 @@ class TestFlaskAPI(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
-    
-    def test_plus(self):
-        response = self.app.get('/plus/5/6')
+
+    def test_prime_true(self):
+        response = self.app.get('/is_prime/11')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode('utf-8'), '11')
+        self.assertEqual(response.data.decode('utf-8'), 'true')
+
+    def test_prime_false(self):
+        response = self.app.get('/is_prime/12')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), 'false')
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main()    
+    # def test_plus(self):
+    #     response = self.app.get('/plus/5/6')
+    #     self.assertEqual(response.status_code, 200)
+        # self.assertEqual(response.data.decode('utf-8'), '11')
+
+# if __name__ == '__main__':
+#     unittest.main()
