ac4f1e32f18a98626db4b854c5f4b881e8a8a8ae
Tan Wattikorn <77262463+Tantalumm@users.noreply.github.com>
2025-02-26 13:52:28 +0700

    update

--- app.py
+++ app.py
@@ -2,25 +2,13 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/is_prime/<int:x>', methods=['GET'])
-def is_prime(x):
-    if x <= 1:
-        return 'false'
-    for i in range(2, int(x**0.5) + 1):
-        if x % i == 0:
-            return 'false'
-    return 'true'
+@app.route('/cir_sur/<x>', methods=['GET'])
+def cir_sur(x:str):
+    r = float(x)
+    if r <= 0 :
+        return "0.00"
+    ans = 4*(3.14)*(r*r)
+    return str(round(ans,2))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 8081)
-
-# @app.route('/getcode', methods=['GET'])
-# def get_code():
-#     return "0000"
-
-# @app.route('/plus/<int:a>/<int:b>', methods=['GET'])
-# def plus(a, b):
-#     return str(a + b), 200
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=8081)
+    app.run(host='0.0.0.0', port=8081)
--- test.py
+++ test.py
@@ -7,15 +7,21 @@ class TestFlaskAPI(unittest.TestCase):
         self.app = app.test_client()
         self.app.testing = True
 
-    def test_prime_true(self):
-        response = self.app.get('/is_prime/11')
+    def test_x_is_1(self):
+        response = self.app.get('/cir_sur/1')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode('utf-8'), 'true')
+        self.assertEqual(response.data.decode('utf-8'), '12.56')
 
-    def test_prime_false(self):
-        response = self.app.get('/is_prime/12')
+    def test_x_is_neg10(self):
+        response = self.app.get('/cir_sur/-10')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode('utf-8'), 'false')
+        self.assertEqual(response.data.decode('utf-8'), '0.00')
+
+
+    def test_x_is_1dot5(self):
+        response = self.app.get('/cir_sur/1.5')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), '28.26')
 
 if __name__ == '__main__':
     unittest.main()    
